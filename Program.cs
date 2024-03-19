3b2a6e99637dd466f3adbc24c92c1192345b9fbb
Aman Ullah <amanullah43085@gmail.com>
2024-03-20 03:00:15 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using RomanToIntegar;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Solution solution = new Solution();
+
+        while (true)
+        {
+            Console.WriteLine("+++++++++++++++++++++++++++");
+            Console.WriteLine("Enter a Roman Numerals");
+            string input = Console.ReadLine().Trim();
+
+            if (input.ToLower() == "exit")
+                break;
+
+
+            int result = solution.RomanToInt(input);
+            if (result != -1)
+            {
+                Console.WriteLine($"The integar value of {input} is : {result}");
+            }
+            else
+            {
+                
+                Console.WriteLine("Please enter a valid Roman Numeral!!");
+            }
+        }
+        
+    }
+}
---
+++ Solution.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomanToIntegar
+{
+    public class Solution
+    {
+        public int RomanToInt(string s)
+        {
+            s = s.ToUpper();
+            Dictionary<char, int> romanValues = new Dictionary<char, int>() {
+            {'I', 1},
+            {'V', 5},
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000}
+        };
+
+            int result = 0;
+            int prevValue = 0;
+
+            for (int i = s.Length - 1; i >= 0; i--)
+            {
+                char currentChar = s[i];
+                if (!romanValues.ContainsKey(currentChar))
+                {
+                    Console.WriteLine($"Invalid Roman numeral '{currentChar}'");
+                    return -1;
+                }
+                int value = romanValues[currentChar];
+
+                if (value < prevValue)
+                {
+                    result -= value;
+                }
+                else
+                {
+                    result += value;
+                }
+
+                prevValue = value;
+            }
+
+            return result;
+        }
+        
+    }
+}
